13eb55acf9bf09b47935e9b5c2dba37875231e87
Denis Chuvasov <chuvasov.d@gmail.com>
2025-10-28 14:20:58 +0100

    Initialize Dart project with basic

---
+++ bin/leet_code.dart
@@ -0,0 +1,5 @@
+import 'package:leet_code/leet_code.dart' as leet_code;
+
+void main(List<String> arguments) {
+  print('Hello world: ${leet_code.calculate()}!');
+}
---
+++ test/leet_code_test.dart
@@ -0,0 +1,8 @@
+import 'package:leet_code/leet_code.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
